21291186a3367c26442b116807796c10af566267
maddie <lee.maddie@gmail.com>
2024-04-09 22:37:57 -0400

    Create connection file

---
+++ backend/db/connection.js
@@ -0,0 +1,13 @@
+const { Pool } = require("pg");
+
+const params = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+};
+
+const db = new Pool(params);
+
+module.exports = db;
