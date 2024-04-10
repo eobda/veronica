95cfed0a09d419da8db0ae14d00f202ed3a567cb
maddie <lee.maddie@gmail.com>
2024-04-09 22:06:01 -0400

    Create user seed data for testing

---
+++ backend/db/seeds/01_users.sql
@@ -0,0 +1,17 @@
+INSERT INTO
+  users (username, name, password)
+VALUES
+  (
+    'test1',
+    'Maddie',
+    '12345'
+  );
+
+INSERT INTO
+  users (username, name, password)
+VALUES
+  (
+    'test2',
+    'Kaia',
+    '12345'
+  );
