49b6750f0741ffa679e40811b55e0442b08dfef5
maddie <lee.maddie@gmail.com>
2024-04-09 21:59:21 -0400

    Fix syntax

--- backend/db/schema/01_users.sql
+++ backend/db/schema/01_users.sql
@@ -1,8 +1,9 @@
 DROP TABLE IF EXISTS users CASCADE;
 
-CREATE TABLE users (
-  id serial PRIMARY KEY NOT NULL,
-  username varchar(255) NOT NULL,
-  name varchar(255) NOT NULL,
-  password varchar(255) NOT NULL
-);
+CREATE TABLE
+  users (
+    id SERIAL PRIMARY KEY NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+  );
