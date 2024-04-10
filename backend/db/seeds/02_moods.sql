cc45478e5a0847496b99c80f17e9c4a5a68a674d
maddie <lee.maddie@gmail.com>
2024-04-09 22:29:22 -0400

    Create mood seed data for testing

---
+++ backend/db/seeds/02_moods.sql
@@ -0,0 +1,17 @@
+INSERT INTO
+  moods (name, user_id, created_at)
+VALUES
+  (
+    'happy',
+    1,
+    "2024-04-01 00:00:00"
+  );
+
+INSERT INTO
+  moods (name, user_id, created_at)
+VALUES
+  (
+    'tired',
+    1,
+    "2024-04-02 00:00:00"
+  );
