25d38577e8aa8d8f6cdc39f2eda6ffcac2e3d305
maddie <lee.maddie@gmail.com>
2024-04-09 22:47:25 -0400

    Remove chalk

--- backend/bin/resetdb.js
+++ backend/bin/resetdb.js
@@ -3,36 +3,27 @@ require("dotenv").config();
 
 // other dependencies
 const fs = require("fs");
-const chalk = require("chalk");
 const db = require("../db/connection");
-
-// PG connection setup
-// const connectionString = process.env.DATABASE_URL ||
-//   `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
-// const client = new Client({
-//   connectionString: connectionString
-// });
-// client.connect();
 
 // Loads the schema files from db/schema
 const runSchemaFiles = async () => {
-  console.log(chalk.cyan(`-> Loading Schema Files ...`));
+  console.log(`-> Loading Schema Files ...`);
   const schemaFilenames = fs.readdirSync("./db/schema");
 
   for (const fn of schemaFilenames) {
     const sql = fs.readFileSync(`./db/schema/${fn}`, "utf8");
-    console.log(`\t-> Running ${chalk.green(fn)}`);
+    console.log(`\t-> Running ${fn}`);
     await db.query(sql);
   }
 };
 
 const runSeedFiles = async () => {
-  console.log(chalk.cyan(`-> Loading Seeds ...`));
+  console.log(`-> Loading Seeds ...`);
   const schemaFilenames = fs.readdirSync("./db/seeds");
 
   for (const fn of schemaFilenames) {
     const sql = fs.readFileSync(`./db/seeds/${fn}`, "utf8");
-    console.log(`\t-> Running ${chalk.green(fn)}`);
+    console.log(`\t-> Running ${fn}`);
     await db.query(sql);
   }
 };
@@ -48,7 +39,7 @@ const runResetDB = async () => {
     await runSeedFiles();
     process.exit();
   } catch (err) {
-    console.error(chalk.red(`Failed due to error: ${err}`));
+    console.error(`Failed due to error: ${err}`);
     process.exit();
   }
 };
