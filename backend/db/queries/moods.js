543b72f3e3f414842d7a9e1f2b125234fc1491be
maddie <lee.maddie@gmail.com>
2024-04-11 18:44:15 -0400

    Write basic mood insert query

--- backend/db/queries/moods.js
+++ backend/db/queries/moods.js
@@ -1,1 +1,17 @@
 const db = require('../connection');
+
+// Insert mood to db
+const addMood = function(mood) {
+  const queryString = `INSERT INTO moods (name, user_id, date_added) VALUES ($1, $2, $3) RETURNING name;`;
+  const queryParams = [mood.name, mood.user_id, mood.date];
+
+  return db.query(queryString, queryParams)
+    .then((data) => {
+      return data.rows[0];
+    })
+    .catch((error) => {
+      console.log(`Error in addMood: ${error.message}`);
+    })
+};
+
+module.exports = { addMood };
