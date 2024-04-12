5827d73c67bc3e63ffb79c7983bf85fc1fa60dc8
maddie <lee.maddie@gmail.com>
2024-04-12 12:36:40 -0400

    Create POST route to add mood

--- backend/routes/moods.js
+++ backend/routes/moods.js
@@ -1,3 +1,13 @@
 const axios = require('axios');
 const db = require('../db/connection');
 const router = require('express').Router();
+
+// ADD mood
+router.post("/", async (req, res) => {
+  try {
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
