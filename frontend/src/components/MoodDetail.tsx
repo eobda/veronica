0d857296a4ff9b6c06fe451c14e323fcc683b5e3
maddie <lee.maddie@gmail.com>
2024-04-18 16:12:19 -0400

    Add mood variable

--- frontend/src/components/MoodDetail.tsx
+++ frontend/src/components/MoodDetail.tsx
@@ -1,7 +1,11 @@
 function MoodDetail({ today }: { today: string }) {
+
+  const mood: string = 'chill' // test mood
+
   return (
     <>
-      Today is {`${today}`}.
+      Today is {`${today}`}.<br />
+      I feel {`${mood}`}.
     </>
   )
 }
