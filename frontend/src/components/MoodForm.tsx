b1f5200cc34180d449bf70f3c1eb557b0eb6dc11
maddie <lee.maddie@gmail.com>
2024-04-19 09:54:59 -0400

    Add error handling

--- frontend/src/components/MoodForm.tsx
+++ frontend/src/components/MoodForm.tsx
@@ -14,6 +14,7 @@ function MoodForm({ today, setTodayMood }: { today: string, setTodayMood: (today
   const onSubmit: SubmitHandler<Inputs> = (formData) => {
     axios.post('http://localhost:8080/api/moods', formData)
       .then((response) => setTodayMood(response.data.name))
+      .catch((error) => console.log(error))
   }
 
   return (
