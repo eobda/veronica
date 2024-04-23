3ee41885b1111b04389d98a4234a96436a600cb6
maddie <lee.maddie@gmail.com>
2024-04-22 22:55:45 -0400

    Add seed data for testing month ranges

--- backend/db/seeds/02_moods.sql
+++ backend/db/seeds/02_moods.sql
@@ -15,3 +15,21 @@ VALUES
     1,
     CURRENT_DATE - 2
   );
+
+INSERT INTO
+  moods (name, user_id, date_added)
+VALUES
+  (
+    'frustrated',
+    1,
+    CURRENT_DATE - 30
+  );
+
+INSERT INTO
+  moods (name, user_id, date_added)
+VALUES
+  (
+    'content',
+    1,
+    CURRENT_DATE - 29
+  );
