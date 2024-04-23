e06f2d0977791f2ad243af74c24e28f2c532039e
maddie <lee.maddie@gmail.com>
2024-04-22 22:47:15 -0400

    Create MoodArchive page

---
+++ frontend/src/components/MoodArchive.jsx
@@ -0,0 +1,14 @@
+/*
+- use .map to make table for moods returned by a query
+*/
+
+function MoodArchive() {
+  return (
+    <>
+      <h1>Mood Archive</h1>
+      <table><th>Moods</th></table>
+    </>
+  )
+}
+
+export default MoodArchive
