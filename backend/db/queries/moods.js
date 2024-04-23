8d4abcc761fb749ac88ed8f54ffe6dc7f30eba41
maddie <lee.maddie@gmail.com>
2024-04-22 23:09:57 -0400

    Create getMoodByMonth function

--- backend/db/queries/moods.js
+++ backend/db/queries/moods.js
@@ -12,6 +12,26 @@ const getMoodByDateAdded = function(userID) {
   })
 };
 
+// Get all moods for a certain MONTH in db
+// dateRange should be an array - [0] is the minimum date and [1] is the max
+const getMoodByMonth = function(userID, dateRange) {
+  const queryString = `SELECT * FROM moods
+    WHERE user_id = $1
+    AND date_added >= $2
+    AND date_added < $3;`;
+  const queryParams = [userID, dateRange[0], dateRange[1]];
+
+  return db
+    .query(queryString, queryParams)
+    .then((data) => {
+      console.log(data.rows[0]);
+      return data.rows[0];
+    })
+    .catch((error) => {
+      console.log(`Error in getMoodByMonth: ${error.message}`);
+    });
+}
+
 // Insert mood to db
 const addMood = function(mood) {
   const queryString = `INSERT INTO moods (name, user_id, date_added) VALUES ($1, $2, CURRENT_DATE) RETURNING name;`;
@@ -25,7 +45,7 @@ const addMood = function(mood) {
     })
     .catch((error) => {
       console.log(`Error in addMood: ${error.message}`);
-    })
+    });
 };
 
 module.exports = { addMood, getMoodByDateAdded };
