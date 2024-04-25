5e526e04735d3b17b3efb699b1e0b36feecab622
maddie <lee.maddie@gmail.com>
2024-04-24 22:16:52 -0400

    Use useState to set date for MoodList

--- frontend/src/components/MoodArchive.tsx
+++ frontend/src/components/MoodArchive.tsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import MoodList from "./MoodList";
 
 function MoodArchive() {
+  const [ date, setDate ] = useState<{year: number, month: number}>({ year: 0, month: 0 })
   return (
     <>
       <h1>Mood Archive</h1>
-      <MoodList year={2024} month={4} />
+      <MoodList year={date.year} month={date.month} />
     </>
   )
 }
