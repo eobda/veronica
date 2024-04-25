1645421471e0a55a1855b2e80ea35d6db98038f2
maddie <lee.maddie@gmail.com>
2024-04-24 22:22:12 -0400

    Add function to go to next month or previous month

--- frontend/src/components/MoodArchive.tsx
+++ frontend/src/components/MoodArchive.tsx
@@ -3,10 +3,17 @@ import MoodList from "./MoodList";
 
 function MoodArchive() {
   const [ date, setDate ] = useState<{year: number, month: number}>({ year: 0, month: 0 })
+
+  const goPrevMonth = () => {setDate({...date, month: date.month - 1})}
+  const goNextMonth = () => {setDate({...date, month: date.month + 1})}
+
   return (
     <>
       <h1>Mood Archive</h1>
       <MoodList year={date.year} month={date.month} />
+      <div>
+        <button onClick={() => {goPrevMonth()}}>Last Month</button> ||       <button onClick={() => {goNextMonth()}}>Next Month</button>
+      </div>
     </>
   )
 }
