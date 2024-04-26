73c3cb66caefcaa68e29ffef467635077f47910b
maddie <lee.maddie@gmail.com>
2024-04-25 21:16:01 -0400

    Implement addUser query

--- backend/db/queries/users.js
+++ backend/db/queries/users.js
@@ -1,1 +1,16 @@
 const db = require('../connection');
+
+// Add new user to db
+const addUser = function(user) {
+  const queryString = `INSERT INTO users (username, name, password) VALUES ($1, $2, $3);`
+  const queryParams = [user.username, user.name, user.password];
+
+  return db
+    .query(queryString, queryParams)
+    .then((data) => {
+      return data.rows[0];
+    })
+    .catch((error) => {
+      console.log(`Error in addUser: ${error.message}`);
+    });
+};
