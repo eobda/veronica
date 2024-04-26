b1276f0695c86570c5baf82138a7140c4b11cb94
maddie <lee.maddie@gmail.com>
2024-04-25 22:13:50 -0400

    Create user registration function

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,2 +1,20 @@
 const axios = require('axios');
+const { addUser } = require('../db/queries/users');
 const router = require('express').Router();
+
+// Register user
+router.post("/new", async (req, res) => {
+  try {
+    const newUser = req.body;
+    addUser(newUser)
+      .then((data) => {
+        return data; // set user id in session/jwt
+      })
+      .catch((error) => {
+        console.log('Server error')
+      })
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  };
+});
