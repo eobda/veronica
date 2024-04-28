476f9b94f4fe60fe78c7bd796cdd9bbf5d360326
maddie <lee.maddie@gmail.com>
2024-04-28 13:35:13 -0400

    Create basic register form

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,3 +1,32 @@
-function Register() {}
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+type Inputs = {
+  username: string,
+  name: string,
+  password: string
+}
+
+function Register() {
+  const {
+    register,
+    handleSubmit
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    console.log(formData)
+  }
+
+  return (
+    <>
+    <h2>Sign Up</h2>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Username</label>
+      <input {...register('username')} />
+      <label>Password</label>
+      <input {...register('password')} />
+    </form>
+    </>
+  )
+}
 
 export default Register
