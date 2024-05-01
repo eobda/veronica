87c9eb911ff513257af48fb193646cfb8e9e3ec2
maddie <lee.maddie@gmail.com>
2024-04-30 22:22:10 -0400

    Return id from addUser

--- backend/db/queries/users.js
+++ backend/db/queries/users.js
@@ -2,7 +2,7 @@ const db = require('../connection');
 
 // Add new user to db
 const addUser = function(user) {
-  const queryString = `INSERT INTO users (username, name, password) VALUES ($1, $2, $3);`
+  const queryString = `INSERT INTO users (username, name, password) VALUES ($1, $2, $3) RETURNING id;`
   const queryParams = [user.username, user.username, user.password];
 
   return db
