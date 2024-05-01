cbef3a57b6a20e8a4e9f099d4ccbe5d46daddc73
maddie <lee.maddie@gmail.com>
2024-04-30 22:18:06 -0400

    Maker user's name use username by default

--- backend/db/queries/users.js
+++ backend/db/queries/users.js
@@ -3,7 +3,7 @@ const db = require('../connection');
 // Add new user to db
 const addUser = function(user) {
   const queryString = `INSERT INTO users (username, name, password) VALUES ($1, $2, $3);`
-  const queryParams = [user.username, user.name, user.password];
+  const queryParams = [user.username, user.username, user.password];
 
   return db
     .query(queryString, queryParams)
