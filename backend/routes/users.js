6f0ce72118344d4ea85db0c4d47e43147b0c2aba
maddie <lee.maddie@gmail.com>
2024-05-01 23:03:30 -0400

    Send response from api/users/new route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,14 +5,8 @@ const router = require('express').Router();
 // Register user
 router.post("/new", async (req, res) => {
   try {
-    const newUser = req.body;
-    addUser(newUser)
-      .then((data) => {
-        return data; // set user id in session/jwt
-      })
-      .catch((error) => {
-        console.log('Server error')
-      })
+    const newUser = await addUser(req.body);
+    res.status(201).send(newUser);
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Server error');
